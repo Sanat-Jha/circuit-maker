dec579b6d0f2f203d4b7e5daf8a97cd305dcd545
sanat <sanatjha4@gmail.com>
2021-05-27 11:09:09 +0530

    Main

--- circuit_maker/wsgi.py
+++ circuit_maker/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'circuit_maker.settings')
