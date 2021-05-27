c23ad9fd413dfde611b7a71e59e186918214813c
sanat <sanatjha4@gmail.com>
2021-05-27 10:53:00 +0530

    Main

--- circuit_maker/settings.py
+++ circuit_maker/settings.py
@@ -126,4 +126,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
