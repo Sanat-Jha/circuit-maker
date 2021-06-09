0ddcf8e995bc41e76116f5dd40552db3fb480fa7
sanat <sanatjha4@gmail.com>
2021-06-09 13:04:47 +0530

    Main

--- circuit_maker/views.py
+++ circuit_maker/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .circuitmaker import makecircuit
 import json
+import time
+# from django.views.csrf import csrf_exempt
 def home(request):
     return render(request,"index.html")
 
 def getsvg(request):
-    circuit = request.POST.get('circuitlst')
+    if request.method == "POST":
+        circuit = request.POST.get('circuitlst')
+    else:
+        circuit = request.GET.get('circuitlst')
     circuit = json.loads(circuit)
     circuitsvg = makecircuit(circuit)
     circuitsvg = str(circuitsvg)
